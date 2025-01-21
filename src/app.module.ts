72d5df8c53a0118a1b798ea3ff0ee6155a22daff
Uriel Ventura <36737847+UrielVentura@users.noreply.github.com>
2025-01-20 20:30:40 -0400

    add Docker support and configure PostgreSQL with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // 'mongodb' if you are using mongodb
+      host: 'db', //  name of the service in docker-compose.yml
+      port: 5432, // 27017 if you are using mongodb
+      username: 'user',
+      password: 'password',
+      database: 'product_db',
+      entities: [],
+      synchronize: true, // warning! do not use on production
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
